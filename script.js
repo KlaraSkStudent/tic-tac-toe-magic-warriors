b8fc4fe4ca12387249c31fc0ed87a57ed68425aa
Saga Eriksson <sagaemilia000@gmail.com>
2025-01-16 12:57:04 +0100

    Finished

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let oGameData = {};
 
 // Här skapar vi funktionen som Jesper visade under lektionen, som kan användas för att skriva ut saker till konsolen.
 const log = (msg) => console.log(msg);
-
+//if-sats som kontrollerar if game over, hit skickar vi ett 1,2,3 eller 0 värde beroende på utfall
+// från checkforgameover
 window.addEventListener("load", () => {
   initGlobalObject();
   if (checkForGameOver() === 1) {
@@ -30,7 +31,7 @@ window.addEventListener("load", () => {
 function initGlobalObject() {
   //Datastruktur för vilka platser som är lediga respektive har brickor
   //Genom at fylla i här med antingen X eler O kan ni testa era rättningsfunktioner
-  oGameData.gameField = ["X", "X", "X", "", "", "", "", "", ""];
+  oGameData.gameField = ['O', '', '', '', '', '', '', '', ''];
 
   /* Testdata för att testa rättningslösning */
   //oGameData.gameField = ['X', 'X', 'X', '', '', '', '', '', ''];
@@ -84,53 +85,101 @@ function initGlobalObject() {
 function checkForGameOver() {
   log("Om det här syns har checkForGameOver() startat");
 
-  checkWinner();
+  // Den här funktionen ska returnera 1, 2 eller 3
+  // Anropar checkwinner med argumentet playerOne, om vinnare returnera 1
+  // Anropar checkwinner med argumentet playerTwo, om vinnare returnera 2
+  // Vid ingen vinnare anropas checkForDraw och vid true returnera 3
+  // Annars returneras 0 och spelet fortsätter
+   
+  
+  let result;
+   
+  if(checkWinner(oGameData.playerOne)== true){
+    result = 1
+    log(result)
+  }else if(checkWinner(oGameData.playerTwo) == true){
+    result = 2
+    log(result)}
+  else if (checkForDraw() == true) {
+    result = 3
+    log(result)}
+  else {
+    result= 0    
+    log(result)
+  }
+  return result
+
 }
 
-// Säg till om ni vill få pseudokod för denna funktion
-// Skapa en array innehållande alla vinnande kombinationer. Varje vinnande kombiation är själv en array enligt följande [1, 2, 3] där siffrorna representerar en plats i gameField.
-// Skapa en flagga (boolean) för isWinner och sätt den till false.
+// Skapa en array innehållande alla vinnande kombinationer. Varje vinnande kombiation är själv en array enligt följande [1, 2, 3] där siffrorna representerar en plats i gameField. CHECK
+// Skapa en flagga (boolean) för isWinner och sätt den till false. CHECK
 // Loopa igenom varje vinnande kombination
 // Skapa variablerna a, b, c och tilldela dem värdena i gameField för din nuvarande vinnande kombination.
 // Titta sedan om a, b, eller c alla är lika med playerIn, i så fall har vi en vinnare, annars inte.
 // Viktigt att funktionen returnerar true eller false baserat på om den inskickade spelaren är winner eller ej
 
+
 function checkWinner(playerIn) {
   log("Om det här syns har checkWinner() startat");
 
-  checkForDraw();
-
   let isWinner = false;
+
   let winningCombinations = [
-    // Om alla de här är X - gör något
-    // Om alla de här är O - gör något
     //Vinnande vågräta rader
-    // [1, 2, 3],
-    // [4, 5, 6],
-    // [7, 8, 9],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
     //Vinnande lodräta rader
-    // [1,4,7]
-    // [2,5,8]
-    // [3,6,9]
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
     //Vinnande diagonala rader
-    // [1,5,9]
-    // [3,5,7]
+    [0, 4, 8],
+    [2, 4, 6],
   ];
+
+  //Loopa igenom varje vinnande kombination. Här går vi in i varje array(vinstkombo) i arrayen winningCombinations
+  for (const combination of winningCombinations) {
+    
+    // abc är parametrar som i varje loop byts ut mot en array i arrayen winningCombinations
+    //a b c byts ut mot varje array (t ex [0,1,2]) i winningCombinations
+    const [a, b, c] = combination;
+    log(`Vinstchans: ${combination}`);
+
+    // Om kontrollraden matchar med playerIn ändras variabeln isWinner till true
+    // ogameData.gameField[a]=0, [b]=1, [c]=2 om vi utgår från exemplet ovan, här kontrolleras
+    // player inputs och rättas mot winningCombinations, vid 3 på rad blir isWinner=true
+    // om vi inte hittar 3 på rad så loopar vi igenom nästa array i winningCombinations tills
+    // alla möjliga winningCombinations är testade
+    // Om ingen vinnande kombination hittas returneras isWinner = false
+    if (oGameData.gameField[a] === playerIn &&
+            oGameData.gameField[b] === playerIn &&
+           oGameData.gameField[c] === playerIn) {
+          
+       isWinner = true;
+      
+    }
+   
+  }
+  return isWinner
 }
-//så länge det är färre än 3 drag för någon av spelarna händer ingenting.
-//Fundering från Klara: enligt det som kommer ut i konsolen kollar vi just nu om det finns en vinnare efter varje runda, vilket borde vara mer rätt än det som står på raden ovan...? Alltså att man hela tiden kollar efter varje omgång och när någon av spelarna har 3 i rad kommer en vinnare utses.
-//när någon av spelarna har nått 3 drag kontrolleras spelplanen mot en lista av kolumner, rader & diagonaler
-//
-//Kontrollera om alla platser i oGameData.GameField är fyllda. Om sant returnera true, annars false.
+
+
+
 
 function checkForDraw() {
   log("Om det här syns har checkForDraw() startat");
 
-  //Här började Jacob förklara att vi behövde loopa igenom arrayen ifall vi ville skriva ut innehållet. Vi hann inte riktigt prata klart innan vi började prata om code review etc.
-  for (let i = 0; i < oGameData.length; i++) {
-    console.log(oGameData.gameField[i]);
+  //some() kontrollerar om det finns någon tom sträng(ledig plats) i vår testrad.
+  //Den här funktionen anropas i checkForGameOver(). Om checkForGameOver får ett truevärde från den här funktionen blir den till en 3:a som i sin tur skickas vidare till checkForGameover
+  if (oGameData.gameField.some(cell => cell === "")) {
+    return false;
   }
+
+  // Om det inte finns vinnare och inga tomma celler är det oavgjort
+  return true;
 }
+
 
 // Nedanstående funktioner väntar vi med!
 
